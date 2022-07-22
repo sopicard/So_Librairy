f3aa5ade569f7b8566cc635b160ef43aab509ffe
sopicard <sogautrelet@gmail.com>
2022-07-22 15:52:49 +0200

    Burger menu with Damien's touch

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,20 +3,20 @@
 //      console.log('test js');
 //Penser  à VIDER LE CACHE (ctrl F5 ?) sur le navigateur pour lui effacer mémoire CSS
 
-//2- Je cible le document et je met un écouteur d'évènement au "click"
+//2- Cibler le document et mettre un écouteur d'évènement au "click"
 //      document.addEventListener('click', function (){
 //
 // });
 
-//10- Je cible le body
+//10- Cibler le body
 const body = document.querySelector('.js_body');
-//11- Si le night mode est activé dans le localstorage => verif avec true ...
+//11- Si le night mode est activé dans le localstorage => vérif avec true ...
 if (localStorage.getItem('nightActivated') === 'true') {
-    //12- ... alors je l'active avec le scss
+    //12- ... alors je l'active avec le css
     body.classList.add('night-activated');
 }
 
-//3- Je crée une variable qui cible mon btn night mode grâce à sa classe JS
+//3- Création d'une variable qui cible mon btn night mode grâce à sa classe JS
 //      et je peux tester avec un console.log(nightBtn) en commentant le 2.
 const nightBtn = document.querySelector('.js_night_mode');
 
@@ -32,15 +32,27 @@ nightBtn.addEventListener('click', function() {
        //13- Et je supprime le night-mode du localstoarge
        localStorage.removeItem('nightActivated')
     }else {
-        //9- je mets mon point 6 dans mon else
+        //9- Je mets mon point 6 dans mon else
         body.classList.add('night-activated');
         //14- Et j'enregistre le night-mode dans le localstorage
         localStorage.setItem('nightActivated', "true");
     }
 });
-//6- A ma var body(js) j'ajoute une class css
+//6- A ma var body(JS) j'ajoute une class css
 //    body.classList.add('night-activated');
 
 //7- Je mets ma classe night-activated dans mon _header.scss
 //     si je souhaite que les paramètres sccs des classes JS passent en priorité,
 //     je peux ajouter !important juste après la valeur (ex: background-color: lightpink !important;)
+
+//12- Création 2 ariables qui ciblent le bouton de la nav mobile et le nav mobile.
+const hamburgerButton = document.querySelector(".nav-toggler")
+const navigation = document.querySelector(".menu-mobile")
+
+//13- Evènement au click avec fonction toggle qui, si la classe existe il l'a supprime sinon il l'a crée.
+hamburgerButton.addEventListener("click",toggleNav)
+
+function toggleNav(){
+    hamburgerButton.classList.toggle("active")
+    navigation.classList.toggle("active")
+}
